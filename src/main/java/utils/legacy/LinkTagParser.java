2affcae9493120cc08fdb0e795e70dae14e07b2a
David George <david.george@gmail.com>
2018-12-05 00:34:27 +0100

    follow redirects when checking urls

--- src/main/java/utils/legacy/LinkTagParser.java
+++ src/main/java/utils/legacy/LinkTagParser.java
@@ -12,66 +12,82 @@
  */
 package utils.legacy;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class LinkTagParser extends WikiLinkHandler {
-    private final Log _logger = LogFactory.getLog(this.getClass());
+	private final Log _logger = LogFactory.getLog(this.getClass());
 
+	public void parseWikiTag(List<String> links, String link) {
+		String url;
+		int pipe = link.indexOf('|');
+		if (pipe > 0) {
+			url = link.substring(0, pipe);
+			link = link.substring(pipe + 1);
+		} else {
+			url = link;
+		}
 
-    public void parseWikiTag(List<String> links, String link) {
-        String url;
-        int pipe = link.indexOf('|');
-        if (pipe > 0) {
-            url = link.substring(0, pipe);
-            link = link.substring(pipe + 1);
-        } else {
-            url = link;
-        }
+		// TODO wouldn't url encode do this?
+		if (!(url.startsWith("http:") || url.startsWith("http:"))) {
+			url = WikiParser.cleanURLText(url) + ".htm";
+		}
 
-        // TODO wouldn't url encode do this?
-        if (!(url.startsWith("http:") || url.startsWith("http:"))) {
-            url = WikiParser.cleanURLText(url) + ".htm";
-        }
+		URL remote = null;
+		try {
+			remote = new URL(url);
+			if (doesURLExist(remote)) {
+				links.add("<a href=\"" + url + "\">" + link + "</a>");
+			} else {
+				links.add(link);
+			}
+		} catch (MalformedURLException e) {
+			_logger.debug("Malformed url " + url);
+			links.add(link);
+		} catch (IOException e) {
+			_logger.debug("io exception " + url);
+			links.add(link);
+		}
+	}
 
-        URL remote = null;
-        try {
-            remote = new URL(url);
-            if (doesURLExist(remote)) {
-                links.add("<a href=\"" + url + "\">" + link + "</a>");
-            } else {
-                links.add(link);
-            }
-        } catch (MalformedURLException e) {
-            _logger.debug("Malformed url " + url);
-            links.add(link);
-        } catch (IOException e) {
-            _logger.debug("io exception " + url);
-            links.add(link);
-        }
-    }
+	public static boolean doesURLExist(URL url) throws IOException {
+		// if (true) return true;
+		// We want to check the current URL
+		HttpURLConnection.setFollowRedirects(false);
 
-    public static boolean doesURLExist(URL url) throws IOException {
-        if (true) return true;
-        // We want to check the current URL
-        HttpURLConnection.setFollowRedirects(false);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url
+				.openConnection();
 
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		// We don't need to get data
+		httpURLConnection.setRequestMethod("HEAD");
 
-        // We don't need to get data
-        httpURLConnection.setRequestMethod("HEAD");
+		// Some websites don't like programmatic access so pretend to be a
+		// browser
+		httpURLConnection
+				.setRequestProperty(
+						"User-Agent",
+						"Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+		int responseCode = httpURLConnection.getResponseCode();
+		System.out.println(">>> " + url + " code " + responseCode);
 
-        // Some websites don't like programmatic access so pretend to be a browser
-        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
-        int responseCode = httpURLConnection.getResponseCode();
+		// normally, 3xx is redirect
 
-        // We only accept response code 200
-        return responseCode == HttpURLConnection.HTTP_OK;
-    }
+		if (responseCode != HttpURLConnection.HTTP_OK
+				&& (responseCode == HttpURLConnection.HTTP_MOVED_TEMP
+						|| responseCode == HttpURLConnection.HTTP_MOVED_PERM || responseCode == HttpURLConnection.HTTP_SEE_OTHER)) {
+			// get redirect url from "location" header field
+			String newUrl = httpURLConnection.getHeaderField("Location");
+			System.out.println("Redirect to URL : " + newUrl);
+
+		}
+		// We only accept response code 200
+		return responseCode == HttpURLConnection.HTTP_OK;
+
+	}
 }
