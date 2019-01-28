14affda49d5c44e4f0742a78b8ef5be86bfa63f1
David George <david.george@gmail.com>
2019-01-29 00:15:41 +0100

    fix snippet code

--- src/main/java/org/magneato/utils/StringHelper.java
+++ src/main/java/org/magneato/utils/StringHelper.java
@@ -13,102 +13,113 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringHelper {
-    private static final ObjectMapper mapper = new ObjectMapper();
-    private static final Logger log = LoggerFactory.getLogger(StringHelper.class);
+	private static final ObjectMapper mapper = new ObjectMapper();
+	private static final Logger log = LoggerFactory
+			.getLogger(StringHelper.class);
 
-    // https://github.com/slugify/slugify
-    private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
-    private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
-    private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
+	// https://github.com/slugify/slugify
+	private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
+	private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
+	private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
 
-    public static String toSlug(String input) {
-        String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
-        String normalized = Normalizer.normalize(nowhitespace,
-                Normalizer.Form.NFD);
-        String slug = NONLATIN.matcher(normalized).replaceAll("");
-        slug = EDGESDHASHES.matcher(slug).replaceAll("");
-        return slug.toLowerCase(Locale.ENGLISH);
-    }
+	public static String toSlug(String input) {
+		String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
+		String normalized = Normalizer.normalize(nowhitespace,
+				Normalizer.Form.NFD);
+		String slug = NONLATIN.matcher(normalized).replaceAll("");
+		slug = EDGESDHASHES.matcher(slug).replaceAll("");
+		return slug.toLowerCase(Locale.ENGLISH);
+	}
 
-    /**
-     * Return a snippet end tag or first 100 characters of string
-     *
-     * @param snippet - string to snip
-     * @param max     - max length of snippet
-     * @return
-     */
-    public static String getSnippet(String snippet, int max) {
-        // non greedy match of first paragraph, note regexp are not perfect for processing html but good enough in most cases
-        Pattern p = Pattern.compile("<\\s*p[^>]*>([^<]*)((<br ?\\/?>)|(<\\s*\\/\\s*[p]\\s*>))");
-        Matcher m = p.matcher(snippet);
+	/**
+	 * Return a snippet end tag or first 100 characters of string
+	 * 
+	 * @param snippet
+	 *            - string to snip
+	 * @param max
+	 *            - max length of snippet
+	 * @return
+	 */
+	public static String getSnippet(String snippet, int max) {
+		// non greedy match of first paragraph, note regexp are not perfect for
+		// processing html but good enough in most cases
+		Pattern p = Pattern
+				.compile("<[^>]*>");
+		Matcher m = p.matcher(snippet);
+		snippet = m.replaceAll("");
 
-        if (m.find()) {
-            snippet = m.group(1);
-        }
+		if (snippet.length() > max) {
+			// cut at sentence boundary
+			int i = snippet.indexOf('.', max)+1;
+			if (i != -1) {
+				snippet = snippet.substring(0, i);
+			} else {
+				snippet = snippet.substring(0, max);
+			}
+		}
 
-        if (snippet.length() > max) {
-            // shorten snippet
-            snippet = snippet.substring(0, snippet.lastIndexOf(' '));
-        }
+		return snippet;
+	}
 
-        return snippet;
-    }
+	/**
+	 * tags are: {image:0} - reference to attached file
+	 * 
+	 * @param snippet
+	 * @param node
+	 * @return
+	 */
+	public static String parseTags(String snippet, JsonNode node) {
+		Pattern p = Pattern.compile("\\{[a-z:0-9]+\\}");
+		Matcher m = p.matcher(snippet);
+		StringBuffer sb = new StringBuffer();
+		JsonNode files = node.get("files");
+		if (files == null) {
+			return snippet;
+		}
 
-    /**
-     * tags are: {image:0} - reference to attached file
-     *
-     * @param snippet
-     * @param node
-     * @return
-     */
-    public static String parseTags(String snippet, JsonNode node) {
-        Pattern p = Pattern.compile("\\{[a-z:0-9]+\\}");
-        Matcher m = p.matcher(snippet);
-        StringBuffer sb = new StringBuffer();
-        JsonNode files = node.get("files");
-        if (files == null) {
-            return snippet;
-        }
+		// consume tags
+		while (m.find()) {
+			String tag = m.group(0);
+			int i = tag.indexOf(':');
+			if (i == -1) {
+				log.error("Invalid tag format {}", tag);
+				continue; // process next tag
+			}
+			try {
+				int index = Integer.parseInt(tag.substring(i + 1,
+						tag.length() - 1));
+				if (index <= files.size()) {
+					String url = files.get(index).get("url").asText();
+					// TODO check ending - can be .jpeg, .jpg, .png, .gif for
+					// image types
+					m.appendReplacement(sb,
+							"<img class=\"img-responsive\" src=\"" + url
+									+ "\"/>");
+				} else {
+					log.error("tag index out of range {}", tag);
 
-        // consume tags
-        while (m.find()) {
-            String tag = m.group(0);
-            int i = tag.indexOf(':');
-            if (i == -1) {
-                log.error("Invalid tag format {}", tag);
-                continue; // process next tag
-            }
-            try {
-                int index = Integer.parseInt(tag.substring(i + 1, tag.length() - 1));
-                if (index <= files.size()) {
-                    String url = files.get(index).get("url").asText();
-                    // TODO check ending - can be .jpeg, .jpg, .png, .gif for image types
-                    m.appendReplacement(sb, "<img class=\"img-responsive\" src=\"" + url + "\"/>");
-                } else {
-                    log.error("tag index out of range {}", tag);
+				}
+			} catch (NumberFormatException e) {
+				log.error("tag bad index format {}", tag);
+				continue; // process next tag
+			}
+		}
+		m.appendTail(sb);
 
-                }
-            } catch (NumberFormatException e) {
-                log.error("tag bad index format {}", tag);
-                continue; // process next tag
-            }
-        }
-        m.appendTail(sb);
+		return sb.toString();
+	}
 
-        return sb.toString();
-    }
+	public static JsonNode toJsonNode(String json) {
+		JsonNode jsonNode = null;
+		try {
+			ObjectReader reader = mapper.reader();
+			jsonNode = reader.readTree(json);
+		} catch (IOException e) {
+			log.error("Something went wrong reading json " + e.getMessage()
+					+ " " + json);
+		}
 
-    public static JsonNode toJsonNode(String json) {
-        JsonNode jsonNode = null;
-        try {
-            ObjectReader reader = mapper.reader();
-            jsonNode = reader.readTree(json);
-        } catch (IOException e) {
-            log.error("Something went wrong reading json " + e.getMessage()
-                    + " " + json);
-        }
-
-        return jsonNode;
-    }
+		return jsonNode;
+	}
 
 }
